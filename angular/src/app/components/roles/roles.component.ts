a6216a44662e10e93e51e37c36e1dc18b11544d0
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-12 21:25:07 +0530

    first commit

--- angular/src/app/components/roles/roles.component.ts
+++ angular/src/app/components/roles/roles.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-roles',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
-
+  firstName: string="Anmol Kumar";
+  angularVersion="Version 18";
+  version:number=18;
+  isActive :boolean = false;
+  currentDate :Date = new Date();
+  inputType:string="button ";
 }
