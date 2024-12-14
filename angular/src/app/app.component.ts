34a056fd1494bd63ef9e99df44e6cd9f39f7544e
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-14 14:37:00 +0530

    html

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {RolesComponent} from './components/roles/roles.component';
+import {DesignationComponent} from './components/designation/designation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RolesComponent],
+  imports: [RouterOutlet, RolesComponent, DesignationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
