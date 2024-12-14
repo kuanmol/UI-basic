7aed888108a17032129cddac04f94d3ad40d2b9b
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-15 00:47:11 +0530

    angular

--- angular/src/app/components/roles/roles.component.ts
+++ angular/src/app/components/roles/roles.component.ts
@@ -1,16 +1,26 @@
 import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
-  firstName: string="Anmol Kumar";
-  angularVersion="Version 18";
-  version:number=18;
-  isActive :boolean = false;
-  currentDate :Date = new Date();
-  inputType:string="button ";
+  firstName: string = "Anmol Kumar";
+  angularVersion = "Version 18";
+  version: number = 18;
+  isActive: boolean = false;
+  currentDate: Date = new Date();
+  inputType: string = "button ";
+  selectedState: string = " ";
+
+  showWelcomeAlert() {
+    alert("Welcome to the Vue.js App");
+  }
+
+  showMessage(message: string) {
+    alert(message);
+  }
 }
