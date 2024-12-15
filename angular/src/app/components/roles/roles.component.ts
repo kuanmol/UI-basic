cdb8236ff782476bf9a9d7c670cde7936458a194
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-15 13:11:12 +0530

    api

--- angular/src/app/components/roles/roles.component.ts
+++ angular/src/app/components/roles/roles.component.ts
@@ -1,26 +1,45 @@
-import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, inject, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {IRole} from '../../model/interface/role';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule, NgForOf],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  firstName: string = "Anmol Kumar";
-  angularVersion = "Version 18";
-  version: number = 18;
-  isActive: boolean = false;
-  currentDate: Date = new Date();
-  inputType: string = "button ";
-  selectedState: string = " ";
+export class RolesComponent implements OnInit {
 
-  showWelcomeAlert() {
-    alert("Welcome to the Vue.js App");
+  roleList: IRole [] = [];
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.getAllRoles()
   }
 
-  showMessage(message: string) {
-    alert(message);
+  getAllRoles(): void {
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res: any) => {
+      this.roleList = res.data;
+    })
   }
+
+
+  // firstName: string = "Anmol Kumar";
+  // angularVersion = "Version 18";
+  // version: number = 18;
+  // isActive: boolean = false;
+  // currentDate: Date = new Date();
+  // inputType: string = "button ";
+  // selectedState: string = " ";
+  //
+  // showWelcomeAlert() {
+  //   alert("Welcome to the Vue.js App");
+  // }
+  //
+  // showMessage(message: string) {
+  //   alert(message);
+  // }
+
 }
