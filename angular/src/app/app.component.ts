6412882d4f818c2c00b0a7b19cbcf4803de58ac4
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-15 12:20:10 +0530

    directives angular

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {RolesComponent} from './components/roles/roles.component';
-import {DesignationComponent} from './components/designation/designation.component';
+import {MasterComponent} from './components/master/master.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RolesComponent, DesignationComponent],
+  imports: [RouterOutlet, RolesComponent, MasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
