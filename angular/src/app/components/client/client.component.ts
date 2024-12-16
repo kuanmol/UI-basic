9989fce845ea0b10c436063b897bb0cba53d9ef2
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-16 16:01:32 +0530

    boots

--- angular/src/app/components/client/client.component.ts
+++ angular/src/app/components/client/client.component.ts
@@ -1,14 +1,17 @@
 import {Component} from '@angular/core';
 import {NgForOf} from '@angular/common';
+import {Client} from '../../model/class/client';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-client',
   imports: [
-    NgForOf
+    NgForOf, FormsModule, NgForOf,
   ],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
 export class ClientComponent {
-
+  clientObj: Client=new Client();
+  clientList: Client[]=[];
 }
